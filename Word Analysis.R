a5cc7f4592dab6bda14cdc7266e9ecea8e564917
garydeinert <gary.deinert@yahoo.com>
2015-05-11 09:39:04 -0400

    Numerous updates and changes
    Near-final-draft copy.  Working to get approval from client. 5-11-15.

--- Word Analysis.R
+++ Word Analysis.R
@@ -15,22 +15,32 @@ mydata <- tm_map(mydata,removeWords,stopwords("english"))
 mydata <- tm_map(mydata,stemDocument)
 mydata <- tm_map(mydata, PlainTextDocument)     ## need to do this before process
 
+setwd("C:/wordanalysis")
+png(filename = "wordcloud.png", width=480, height=480)
+
 wordcloud(mydata, scale=c(5,0.5), 
           max.words=100,
           random.order=FALSE,
           rot.per=0.35,
           use.r.layout=FALSE,
           colors=brewer.pal(8,"Dark2"))
+dev.off()
 
 mycharv <- as.character(mydata)
 mysent <- get_nrc_sentiment(mycharv)
 x <- mysent[1,]
 plotdf <- melt(x)
+
+png(filename = "sentiment.png", width=480, height=480)
+
 barplot(plotdf$value, 
         col=brewer.pal(8,"Dark2"),
         names.arg = plotdf$variable,
         cex.names = 0.80,
         horiz = TRUE,
         main = "Sentiment Analysis",
-        mar = c(5,6,4,1) + 0.3,
+        mar = c(6,4,4,2) + 0.1,
         las = 1)
+
+dev.off()
+
